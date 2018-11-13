3f234a29b01449959c52868e3d5e6e7b26650990
tei0513 <254207025@qq.com>
2018-11-13 20:54:42 +0800

    数字常量池新增

---
+++ com.Yumei/src/main/java/com/yumei/common/utils/consts/NumberConsts.java
@@ -0,0 +1,69 @@
+package com.yumei.common.utils.consts;
+
+/**
+ * 通用数字常量池。
+ * 
+ * @author Tei
+ * @Date 2018年11月13日
+ */
+public final class NumberConsts {
+	
+	/**
+	 * 隐藏构造函数
+	 */
+	private NumberConsts() {
+		
+	}
+	/**
+	 * 数字常数
+	 * 值为{@value}
+	 */
+	public static final int NUM_1 = 1;
+	/**
+	 * 数字常数
+	 * 值为{@value}
+	 */
+	public static final int NUM_2 = 2;
+	/**
+	 * 数字常数
+	 * 值为{@value}
+	 */
+	public static final int NUM_3 = 3;
+	/**
+	 * 数字常数
+	 * 值为{@value}
+	 */
+	public static final int NUM_4 = 4;
+	/**
+	 * 数字常数
+	 * 值为{@value}
+	 */
+	public static final int NUM_5 = 5;
+	/**
+	 * 数字常数
+	 * 值为{@value}
+	 */
+	public static final int NUM_6 = 6;
+	/**
+	 * 数字常数
+	 * 值为{@value}
+	 */
+	public static final int NUM_7 = 7;
+	/**
+	 * 数字常数
+	 * 值为{@value}
+	 */
+	public static final int NUM_8 = 8;
+	/**
+	 * 数字常数
+	 * 值为{@value}
+	 */
+	public static final int NUM_9 = 9;
+	/**
+	 * 数字常数
+	 * 值为{@value}
+	 */
+	public static final int NUM_10 = 10;
+	
+	
+}
