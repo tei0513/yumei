14ca9be1204b81dbcb18dba6443f511f05ca7816
tei0513 <254207025@qq.com>
2018-11-18 17:42:07 +0800

    完善MapperAOP保存修改切点的方法

--- com.Yumei/src/main/java/com/yumei/common/aop/MapperAop.java
+++ com.Yumei/src/main/java/com/yumei/common/aop/MapperAop.java
@@ -2,13 +2,19 @@ package com.yumei.common.aop;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpSession;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import com.yumei.common.entity.BaseEntity;
+import com.yumei.common.utils.CommonUtil;
 import com.yumei.common.utils.StringChecker;
+import com.yumei.common.utils.consts.NumberConsts;
+import com.yumei.sys.entity.UserDetails;
 
 /**
  * Dao类切面。
@@ -26,10 +32,21 @@ public class MapperAop {
 	 * @param point 切入点
 	 */
 	@Before("execution(* com.yumei.*.dao.*.save*(..)) || execution(* com.yumei.*.dao.*.update*(..))")
+	@Order(NumberConsts.NUM_5)
 	public void setModifyInfo(JoinPoint point) {
 		// 获取方法所有传参
 		Object[] args = point.getArgs();
-	
+		// 获取session
+		HttpSession session = CommonUtil.getRequest().getSession();
+		// 获取登陆用户的信息
+		UserDetails user = (UserDetails) session.getAttribute("user_details");
+		String loginName = "";
+		if (null == user) {
+			loginName = "SYS";
+		} else {
+			loginName = user.getLoginName();
+		}
+		
 		for (Object obj: args) {
 			// 如果是BaseEntity类型的对象则设置相关属性
 			if (obj instanceof BaseEntity) {
@@ -37,13 +54,14 @@ public class MapperAop {
 				
 				// 第一次创建时添加创建人和创建时间
 				if (StringChecker.isBlank(((BaseEntity) obj).getCreateUserName())) {
-					//TODO 设置创建人
 					
 					// 设置创建时间
 					((BaseEntity) obj).setCreateDate(new Date());
+					// 设置创建人
+					((BaseEntity) obj).setCreateUserName(loginName);
 				}
-				//TODO 设置修改人
-				
+				// 设置修改人
+				((BaseEntity) obj).setUpdateUserName(loginName);
 				// 设置修改时间
 				((BaseEntity) obj).setUpdateDate(new Date());
 			}
