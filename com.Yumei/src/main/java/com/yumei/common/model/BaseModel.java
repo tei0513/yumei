da7dafb70610ed7c92464b6c8f2dd42dfc683661
tei0513 <254207025@qq.com>
2018-11-30 22:03:46 +0800

    东西太多不想写message

---
+++ com.Yumei/src/main/java/com/yumei/common/model/BaseModel.java
@@ -0,0 +1,15 @@
+package com.yumei.common.model;
+
+import java.io.Serializable;
+
+/**
+ * Model基类。
+ * 
+ * @author Tei
+ * @Date 2018年11月27日
+ */
+public class BaseModel implements Serializable {
+
+	private static final long serialVersionUID = -7396006880490958930L;
+
+}
